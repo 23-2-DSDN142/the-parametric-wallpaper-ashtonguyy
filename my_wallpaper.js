0075927db7c3419267a105a2f228183f4d1a54a1
ashtonguyy <ashtonguy4413@gmail.com>
2023-08-24 17:40:59 +1200

    Added code

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -4,7 +4,7 @@ let rect_height = 20;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.output_mode(DEVELOP_GLYPH);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
@@ -18,8 +18,65 @@ function wallpaper_background() {
   background(240, 255, 240); //light honeydew green colour
 }
 
-function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+var centerY = height / 2;
+var angle = 0;
+var amplitude = 100;
+var frequency = 0.02;
 
-  //draw here
+function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
+
+  // Draw the soundwave
+  drawSoundwave();
+  
+  noStroke(); // No stroke for logo elements
+
+  // Draw the number "8"
+  fill(255, 100, 100); // Red color
+  ellipse(120, 200, 60, 75);
+  ellipse(120, 150, 60, 75);
+
+  fill(255, 255, 255); // White Color
+  ellipse(120, 200, 30, 37.5);
+  ellipse(120, 150, 30, 37.5);
+
+  // Draw the "0"
+  fill(255, 100, 100); // Red colour
+  ellipse(200, 175, 80, 120);
+  
+  fill(255, 255, 255); // White colour
+  ellipse(200, 175, 40, 80);
+
+  // Draw the final "8"
+  fill(255, 100, 100); // Red colour
+  ellipse(280, 200, 60, 75);
+  ellipse(280, 150, 60, 75);
+
+  fill(255, 255, 255); // White Colour
+  ellipse(280, 200, 30, 37.5);
+  ellipse(280, 150, 30, 37.5);
+
+
+  // Draw the letters "FM"
+  fill(100, 100, 255); // Blue color
+  textSize(72);
+  text("F", 320, 240);
+  text("M", 360, 240);
 }
+
+function drawSoundwave() {
+  stroke(0);
+  noFill();
+  beginShape();
+  for (let x = 0; x < width; x += 5) {
+    let y = sin(angle) * amplitude;
+    vertex(x, y);
+    angle += frequency 
+  }
+  endShape();
+}
+
+function draw() {
+  
+}
+
+
